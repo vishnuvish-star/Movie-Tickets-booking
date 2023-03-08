b31f893fcf357d74531b5daba48c5c7ee3860e9e
vishnu <vishnu981203@gmail.com>
2023-03-08 13:24:41 +0530

    Add events

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,43 @@ const overAllSeats = document.querySelectorAll(".row .seats:not(.occupied)");
 // console.log(overAllSeats);
 const selectMovie = document.getElementById("select-movies");
 // console.log(selectMovie);
-const howMuchTickets = document.getElementById("howMuch");
+let howMuchTickets = document.getElementById("howMuch");
 // console.log(howMuchTickets);
-const totalPrice = document.getElementById("price");
+let totalPrice = document.getElementById("price");
 // console.log(totalPrice);
+
+// variables
+let totalTickets = 0;
+let ticketPrice = 0;
+let totalAmount = 0;
+
+// functions
+function calculate() {
+  let seatsSelected = document.querySelectorAll(".row .selected");
+  totalTickets = seatsSelected.length;
+  totalAmount = totalTickets * ticketPrice;
+  totalPrice.innerText = totalAmount;
+  howMuchTickets.innerText = totalTickets;
+}
+
+// Events
+
+selectMovie.addEventListener("change", (e) => {
+  ticketPrice = Number(e.target.value);
+  /*which movie we selected that movie ticket price is keep in this variable "ticketPrice"  */
+  calculate(); /* ticket price is send to calculate function() */
+});
+
+for (let i = 0; i < overAllSeats.length; i++) {
+  const allSeats = overAllSeats[i];
+
+  //   add event listener to all  selecting seats
+
+  allSeats.addEventListener("click", (e) => {
+    if (e.target.classList.contains("seats")) {
+      e.target.classList.toggle("selected");
+    }
+
+    calculate(); /* call the calculate function for how many tickets we are  selected */
+  });
+}
